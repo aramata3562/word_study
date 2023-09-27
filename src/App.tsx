7814b2aca4fe9a469bc1a8a5c3c9e59df07eda9a
aramakitaketo <aramakitaketo@gmail.com>
2023-09-27 22:51:00 +0900

    ✨ gasで連携

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
 import WordCard from './components/wordCard';
 import words from './data/word.json';
 
@@ -8,16 +9,43 @@ type WordData = {
 };
 
 const App: React.FC = () => {
+  const [currentWordIndex, setCurrentWordIndex] = useState(0)
+  const [showMeaning, setShowMeaning] = useState(false)
+  const [words, setWords] = useState<WordData[]>([])
+
+  useEffect(() => {
+    const endpointUrl = 'https://script.google.com/macros/s/AKfycbx0rnDDPvU3TlIYD4F37d1IUcCn_v39sDcioRJMu5-FGHGYREbTlFOkN5TGx7jOqsF1ZA/exec';
+    fetch(endpointUrl)
+      .then(response => response.json())
+      .then(data => {
+        setWords(data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+    }, [])
+
+  const handleButtonClick = () => {
+    setShowMeaning(!showMeaning);
+  };
+
+  const currentWordData = words[currentWordIndex] || { word: '', meaning: '' };
+
   return (
     <div>
       <h1>単語勉強</h1>
-      {words.map((wordData: WordData, index: number) => (
-        <WordCard key={index} word={wordData.word} meaning={wordData.meaning} />
-      ))}
+      <button onClick={handleButtonClick}>
+      {showMeaning ? 'Hide' : 'Show'}
+        </button>
+        <WordCard
+        word={currentWordData.word}
+        meaning={showMeaning ? currentWordData.meaning : ''}
+      />
+      <button onClick={() => setCurrentWordIndex((prevIndex) => (prevIndex + 1) % words.length)}>
+        Next Word
+      </button>
     </div>
   );
 };
 
 export default App;
-
-// check 
